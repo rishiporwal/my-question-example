7a35e1735d146b3c598778de98c00c2c6c89bcc6
rishiporwal <64612539+rishiporwal@users.noreply.github.com>
2020-11-21 14:53:52 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,1 @@
-if (Ask.AddQuestion("1+1", "2", "3")) {
-    if (Ask.AddQuestion("5*3", "21", "15")) {
-        game.over(false)
-    } else {
-        if (Ask.AddQuestion("sqrt(144)", "14", "12")) {
-            game.over(false)
-        } else {
-            game.over(true)
-        }
-    }
-} else {
-    game.over(false)
-}
+
